ac2eee0d3eae3236c2eb4070f4d5c859ce79e837
Alina <beauty05j6@gmail.com>
2021-08-31 17:01:42 +0800

    加入專案檔案。

---
+++ BS.Mvc.GitExercise/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BS.Mvc.GitExercise.Startup))]
+namespace BS.Mvc.GitExercise
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
